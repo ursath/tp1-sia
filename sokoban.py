94003c3844ad222ab89f1831d475d20b22cc683d
solrodriguezg <solrodriguez@itba.edu.ar>
2025-03-20 21:15:05 -0300

    game map

--- sokoban.py
+++ sokoban.py
@@ -1,13 +1,51 @@
 import os
+import arcade
 
-maps_folder = "maps"
-map_number = input(f"Choose a map ({len(os.listdir(maps_folder))} Maps Available): ")
+SCREEN_TITLE = "Sokoban Game"
+PNG_SIZE = 64
 
-with open(f"{maps_folder}/{map_number}.txt") as f:
-    map_data = f.read().splitlines()
+ELEMENTS = {
+    "#": "./textures/wall.png",   # Pared
+    " ": "./textures/free.png",   # Espacio libre (null)
+    ".": "./textures/goal.png",   # Objetivo
+    "@": "./textures/player.png", # Jugador
+    "$": "./textures/box.png",    # Caja
+    "*": "./textures/box_goal.png" # Caja sobre objetivo
+}
 
-map = []
-for row in map_data:
-    map.append(list(row))
+class SokobanGame(arcade.Window):
+    def __init__(self, map_file):
+        with open(map_file, "r") as f:
+            self.map_data = [list(line.strip()) for line in f.readlines()]
 
-print(map)
+        self.num_rows = len(self.map_data)
+        self.num_cols = len(self.map_data[0])
+        width = self.num_cols * PNG_SIZE
+        height = self.num_rows * PNG_SIZE
+
+        super().__init__(width, height, SCREEN_TITLE)   
+        arcade.set_background_color(arcade.color.BLACK)
+
+        self.textures = {
+            key: arcade.load_texture(img) if img else None
+            for key, img in ELEMENTS.items()
+        }
+
+    def on_draw(self):
+
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                x = col * PNG_SIZE + PNG_SIZE // 2
+                y = (self.num_rows - row - 1) * PNG_SIZE + PNG_SIZE // 2  
+
+                tile = self.map_data[row][col]
+                if tile in self.textures and self.textures[tile]:
+                    rect = arcade.Rect(x-PNG_SIZE//2, x+PNG_SIZE//2, y-PNG_SIZE//2, y+PNG_SIZE//2, PNG_SIZE, PNG_SIZE, x, y)
+
+                    arcade.draw_texture_rect(self.textures[tile], rect)
+
+
+
+if __name__ == "__main__":
+    game = SokobanGame("./maps/1.txt")
+    arcade.run()
